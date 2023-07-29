6954ee9ed4588ed327039baa31cff1f8fe93d4e6
Aksultan <aksultanakn@gmail.com>
2023-07-29 16:46:18 +0600

    fixed the openai_service

--- app/characters/adapters/openai_service.py
+++ app/characters/adapters/openai_service.py
@@ -1,5 +1,3 @@
-# adapters/openai_service.py
-# adapters/openai_service.py
 from typing import Any, Dict
 import os
 from dotenv import load_dotenv
@@ -9,15 +7,15 @@ load_dotenv()
 
 
 class OpenAIService:
-    def __init__(self):
+    def init(self):
         openai.api_key = os.getenv("OPENAI_API_KEY")
 
     def generate_prompt(self, text: str) -> Dict[str, Any]:
-        instruction = "act like a script writer assistant with Hollywood expertise.\
+        instruction = "Act like a script writer assistant with Hollywood expertise.\
             Build a character from the following description: "
         prompt_text = instruction + text
         response = openai.Completion.create(
-            engine="text-davinci-002",
+            engine="gpt-3.5-turbo",
             prompt=prompt_text,
             temperature=0.5,
             max_tokens=1000,
@@ -25,25 +23,25 @@ class OpenAIService:
         return response.choices[0].text.strip()
 
     def imitate_style(self, text: str, author: str) -> Dict[str, Any]:
-        instruction = f"Write a short script in the style of {author} about "
+        instruction = f"Rewrite the following script in the style of the director {author}, do not add too much content: "
         prompt_text = instruction + text
         response = openai.Completion.create(
-            engine="text-davinci-002", 
-            prompt=prompt_text, 
+            engine="gpt-3.5-turbo",
+            prompt=prompt_text,
             temperature=0.5,
-            max_tokens=1000
+            max_tokens=1000,
         )
         return response.choices[0].text.strip()
 
     def fix_imperfections(self, text: str) -> Dict[str, Any]:
-        instruction = "act like a script writer assistant with Hollywood expertise.\
-              Fix the places of text where there is not enough character development: "
+        instruction = "Act like a script writer assistant with Hollywood expertise.\
+              Fix the places of text where there is not enough character development, write very creatively: "
         prompt_text = instruction + text
         response = openai.Completion.create(
-            engine="text-davinci-002",
+            engine="gpt-3.5-turbo",
             prompt=prompt_text,
             temperature=0.5,
-            max_tokens=1000
+            max_tokens=1000,
         )
         return response.choices[0].text.strip()
 
@@ -51,21 +49,21 @@ class OpenAIService:
         instruction = f"Act as a script writer assistant with Hollywood expertise.\
               Generate a detailed stage description for this script act: {act}"
         response = openai.Completion.create(
-            engine="text-davinci-002",
+            engine="gpt-3.5-turbo",
             prompt=instruction,
             temperature=0.5,
-            max_tokens=1000
+            max_tokens=1000,
         )
         return response.choices[0].text.strip()
 
     def finish_thought(self, incomplete_text: str) -> Dict[str, Any]:
         instruction = f"Act as a script writer assistant with Hollywood expertise.\
             Extend the following incomplete thought or idea into a well-developed \
-                  concept: {incomplete_text}"
+                  concept. Add only 1-2 sentences and do NOT change the original text: {incomplete_text}"
         response = openai.Completion.create(
-            engine="text-davinci-002",
+            engine="gpt-3.5-turbo",
             prompt=instruction,
             temperature=0.5,
-            max_tokens=1000
+            max_tokens=1000,
         )
         return response.choices[0].text.strip()
