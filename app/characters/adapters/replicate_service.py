39bb98c3a8bb925a28d3d45b8b9266a071881621
Aksultan <aksultanakn@gmail.com>
2023-08-02 01:51:02 +0600

    updates on replicate

--- app/characters/adapters/replicate_service.py
+++ app/characters/adapters/replicate_service.py
@@ -1,17 +1,18 @@
-# adapters/replicate_service.py
-from typing import Any, Dict
-import replicate
 import os
-
-
-os.environ["REPLICATE_API_TOKEN"] = "r8_ETuXWhy82eXqkVC6p5od1Npt6y6N6F82IWr9P"
+import replicate
+from typing import Any, Dict
 
 
 class ReplicateService:
-    def generate_image(self, prompt: str):
+    def __init__(self):
+        self.replicate_model_key = "prompthero/dreamshaper:6197db9cdf865a7349acaf20a7d20fe657d9c04cc0c478ec2b23565542715b95"
+        self.replicate_api_token = os.getenv('REPLICATE_API_TOKEN')
+
+    def generate_image(self, prompt: str) -> Dict[str, Any]:
+        # Assuming you need to pass the API token to replicate.run
         output = replicate.run(
-            "prompthero/dreamshaper:\
-                6197db9cdf865a7349acaf20a7d20fe657d9c04cc0c478ec2b23565542715b95",
-            input={"prompt": prompt}
+            self.replicate_model_key,
+            input={"prompt": prompt},
+            api_token=self.replicate_api_token
         )
         return output
--- app/characters/router/router_build_character.py
+++ app/characters/router/router_build_character.py
@@ -9,20 +9,11 @@ def build_character(
     text: str,
     svc: Service = Depends(get_service),
 ):
-    # Generate prompt using OpenAI
-    character_description = svc.openai_service.generate_prompt(text)
 
-    # Save to the database
-    result = svc.repository.save_character_description(text, character_description)
-
-    #image_data = svc.replicate_service.generate_image(text)
+    image = svc.replicate_service.generate_image(text)
+    result = svc.repository.save_character_image(text, image)
 
     if result:
-        return {
-            "status": "success",
-            "character description": character_description,
-           # "character image": image_data
-        }
-
+        return {"status": "success", "image": image}
     else:
-        raise HTTPException(status_code=500, detail="Failed to build character")
+        raise HTTPException(status_code=500, detail="Failed to generate character image")
