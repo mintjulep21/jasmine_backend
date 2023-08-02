a71336e4a9aae795ae7101c4eec9e3f67d5e5e43
bph21 <48647702+mintjulep21@users.noreply.github.com>
2023-08-03 05:10:09 +0600

    Update replicate_service.py

--- app/characters/adapters/replicate_service.py
+++ app/characters/adapters/replicate_service.py
@@ -5,7 +5,7 @@ from typing import Any, Dict
 
 class ReplicateService:
     def __init__(self):
-        self.replicate_model_key = "stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4"
+        self.replicate_model_key = "prompthero/dreamshaper:6197db9cdf865a7349acaf20a7d20fe657d9c04cc0c478ec2b23565542715b95"
         self.replicate_api_token = os.getenv('REPLICATE_API_TOKEN')
 
     def generate_image(self, prompt: str) -> Dict[str, Any]:
