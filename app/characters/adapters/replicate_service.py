d8419a74bc11e2fba4c50d7bd99240e4a8e90dc6
bph21 <48647702+mintjulep21@users.noreply.github.com>
2023-08-05 16:51:16 +0600

    Update replicate_service.py

--- app/characters/adapters/replicate_service.py
+++ app/characters/adapters/replicate_service.py
@@ -12,7 +12,7 @@ class ReplicateService:
         # Assuming you need to pass the API token to replicate.run
         output = replicate.run(
             self.replicate_model_key,
-            input={"prompt": prompt + "masterpiece, cinematography, hyper realistic, extremely detailed, extremely detailed face, 4k, award-winning, drama movie", "width": 768, "height": 512, "negative_prompt": "(worst quality:2),(low quality:2),(blurry:2),bad_prompt,text, disfigured, ugly face, (bad and mutated hands:1.3),(bad hands),badhandv4,mutated hands, bad anatomy, missing fingers,extra fingers,fused fingers,too many fingers,(interlocked fingers:1.2), extra limbs,malformed limbs,multiple limbs, extra arms, extra legs, long neck, cross-eyed, negative_hand, negative_hand-neg, text, label, caption, ugly, black and white"},
+            input={"prompt": "extremely detailed face, masterpiece, cinematography, hyper realistic, extremely detailed, 4k, award-winning, drama movie" + prompt, "width": 768, "height": 512, "negative_prompt": "(worst quality:2),(low quality:2),(blurry:2),bad_prompt,text, disfigured, ugly face, (bad and mutated hands:1.3),(bad hands),badhandv4,mutated hands, bad anatomy, missing fingers,extra fingers,fused fingers,too many fingers,(interlocked fingers:1.2), extra limbs,malformed limbs,multiple limbs, extra arms, extra legs, long neck, cross-eyed, negative_hand, negative_hand-neg, text, label, caption, ugly, black and white"},
             api_token=self.replicate_api_token
         )
         return output
